616b73c9e9e558ebdab993db8af0e58b090abce4
Arvid Krein <arvid2000@googlemail.com>
2017-03-21 17:01:15 +0100

    Added Makefile and main program skeleton

---
+++ main.cc
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cc
+ *
+ *    Description:  A simple fighting game
+ *
+ *        Version:  1.0
+ *        Created:  21.03.2017 16:39:34
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Finn Krein, finnkrein@googlemail.com; Arvid Krein, arvid2000@googlemail.com
+ *        Company:  -
+ *
+ * =====================================================================================
+ */
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cmath>
+#include <vector>
+#include "lTexture.h"
+
+int main (/*int argc, char* args[])*/)
+{
+}
