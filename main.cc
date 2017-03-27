3800a00078936471ffd8cdbdfffaea809bf0d780
Arvid Krein <arvid2000@googlemail.com>
2017-03-27 18:04:34 +0200

    updated worldframe class and implemented it in main program.

--- main.cc
+++ main.cc
@@ -28,16 +28,25 @@
 #include "physobj.h"
 #include "bezier.h"
 
+void drawobjs(SDL_Renderer* Renderer, std::vector<kraftpartikel*> vObj);
+int KASTENGROSSE = 5;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
 int main (/*int argc, char* args[])*/)
 {
-	const int SCREEN_WIDTH = 800;
-	const int SCREEN_HEIGHT = 600;
 	SDL_Window* gWindow;
 	SDL_Renderer* gRenderer;
 	init(&gWindow,&gRenderer, SCREEN_HEIGHT, SCREEN_WIDTH);
 	bool quit=false;
 	SDL_Event event;
 	simpleTimer timer;
+	int mousex;
+	int mousey;
+	SDL_Rect drawnRect = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 4 ,4};
+	Worldframe world;
+	world.xsize = SCREEN_WIDTH;
+	world.ysize = SCREEN_HEIGHT;	
 	while (quit!=true)
 	{
 		while(SDL_PollEvent(&event)!=0)
@@ -49,24 +58,52 @@ int main (/*int argc, char* args[])*/)
 				switch (event.key.keysym.sym)
 				{
 					case SDLK_p:
-						timer.pause();
+						timer.flip();
 						break;
-					case SDLK_u:
-						timer.unpause();
-						break;
-					case SDLK_r:
+					case SDLK_o:
 						timer.reset();
 						break;
-					case SDLK_g:
+					case SDLK_i:
 						std::cout << timer.get() << "\n" << std::flush;
 						break;
+					case SDLK_n:
+						world.vKPartikel.push_back(new kraftpartikel(static_cast<double>(mousex), static_cast<double>(mousey)));
 				}	
 
 			}
+
+			if (event.type == SDL_MOUSEBUTTONDOWN)
+			{
+				SDL_GetMouseState(&mousex, &mousey);
+				drawnRect.x=mousex;
+				drawnRect.y=mousey;
+			}
 		}
+		if( timer.get() >= (1000/25) )
+		{
+		world.iterate(0.00001);
 		SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 		SDL_RenderClear(gRenderer);
+		SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
+		drawobjs(gRenderer, world.vKPartikel);
+		SDL_RenderDrawRect(gRenderer, &drawnRect);
 		SDL_RenderPresent(gRenderer);
+		timer.reset();
+
+		}
 	}
 	return 0;
 }
+
+void drawobjs(SDL_Renderer* Renderer, std::vector<kraftpartikel*> vObj)
+{
+	for (auto i : vObj)
+	{
+		SDL_Rect objRect = {0,0,KASTENGROSSE,KASTENGROSSE};
+		objRect.x=i->getx();
+		objRect.y=i->gety();
+		SDL_SetRenderDrawColor(Renderer,0x00,0x00,0x00,0xFF);
+		SDL_RenderFillRect(Renderer,&objRect);
+	}
+}
+
--- physobj.h
+++ physobj.h
@@ -38,8 +38,8 @@ class physobj
 		int setxvel(double x);
 		int setyvel(double y);
 		int reset(double x, double y);
-		physobj(double x, double y);
-				~physobj();
+		physobj(double x, double y, double vx=0, double vy=0);
+		~physobj();
 
 
 	private:
@@ -55,15 +55,50 @@ class physobj
 class kraftpartikel : public physobj
 {
 	public:
-		kraftpartikel(double x, double y, double mass);
+		kraftpartikel(double x, double y, double mass = 1, double charge = 1);
 		void elastischerstoss(kraftpartikel& obj);
+		void iterate(double t);
+		void coulombkraft(kraftpartikel& obj, double kraft);
+		double getFx(); //get methods
+		double getFy();
+		double getMass();
+		double getCharge();
+		void setFx(double F); //set methods
+		void setFy(double F);
+		void setMass(double m);
+		void setCharge(double q);
 
+	private:
 		double Fx;
 		double Fy;
-		int iterate(double t);
-		void coulombkraft(kraftpartikel& obj, double kraft);
-		double m=1;
+		double mMass=1;
+		double mCharge=1;
+
+
+
 };
 
+class Worldframe
+{
+	public:
+		void iterate(double t);
+		void radialForce(kraftpartikel* part1, kraftpartikel* part2, double kraftfaktor, double exponent);
+		void elasticBounce(kraftpartikel* part1, kraftpartikel* part2);
+		void gravitationalForce(kraftpartikel* part, double Fx, double Fy);
+		bool collisioncheck();
+		bool isoutofworld(physobj* part);
+
+		~Worldframe();
+
+		double xsize = 0; 
+		double ysize = 0; 
+		double coulombfaktor = 1;
+		double gravFx = 0;
+		double gravFy = 1;
+
+		std::vector<kraftpartikel*> vKPartikel;
+	private:
+
+};
 
 #endif
